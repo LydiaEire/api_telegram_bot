22dc2745d83c5c468d152db9d0abb6c193fd71be
Лидия <nancyeire1987@gmail.com>
2020-12-08 21:08:50 +0300

    Version 1:logging added

--- homework.py
+++ homework.py
@@ -1,21 +1,30 @@
 import os
 import time
-
+import logging
 import requests
 import telegram
 from dotenv import load_dotenv
 
 load_dotenv()
+logger = logging.getLogger()
+fh = logging.FileHandler('errors.log')
+fh.setLevel(logging.DEBUG)
 
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+
+logger.addHandler(fh)
 
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    if homework_status == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -23,20 +32,20 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    date = {'from_date': current_timestamp}
+    homework_statuses = requests.get(URL, params=date, headers=headers)
     return homework_statuses.json()
 
 
 def send_message(message, bot_client):
-    ...
-    return bot_client.send_message(...)
+    return bot_client.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    # проинициализировать бота здесь
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)  # проинициализировать бота здесь
     current_timestamp = int(time.time())  # начальное значение timestamp
-
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
