8e9b410ee8ff31c8e8d61af5c2b0265687d26078
Лидия <nancyeire1987@gmail.com>
2020-12-12 22:31:27 +0300

    code refactoring + logging added + some code fixes

--- homework.py
+++ homework.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+import logging
 import requests
 import telegram
 from dotenv import load_dotenv
@@ -14,20 +15,38 @@ URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 
 def parse_homework_status(homework):
+    if not isinstance(homework, dict):
+        logging.error('Invalid homework variable %s' % homework)
+        raise ValueError('Invalid homework variable %s' % homework)
+
     homework_name = homework.get('homework_name')
+    if homework_name is None:
+        logging.error('"homework_name" is not found')
+        raise KeyError('"homework_name" is not found')
+
     homework_status = homework.get('status')
+    if homework_status is None:
+        logging.error('"homework_status" is not found')
+        raise KeyError('"homework_status" is not found')
+
+    verdict = 'Статус проверки неизвестен'
     if homework_status == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
+    if homework_status == 'approved':
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
-
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    if current_timestamp is None:
+        current_timestamp = int(time.time())
     date = {'from_date': current_timestamp}
-    homework_statuses = requests.get(URL, params=date, headers=headers)
+    try:
+        homework_statuses = requests.get(URL, params=date, headers=headers)
+    except requests.exceptions.RequestException as e:
+        logging.error(e)
+        raise SystemExit(e)
     return homework_statuses.json()
 
 
@@ -41,7 +60,6 @@ def main():
     current_timestamp = int(time.time())  # начальное значение timestamp
     while True:
         try:
-            print('Проверяем домашку')
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(new_homework.get('homeworks')[0]), bot)
